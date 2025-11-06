fe2a60c9ef0049eacffc453d10db5b71077d7c70
Gabriel Valdez <valdezgabriel010305@gmail.com>
2025-11-06 11:01:49 +0800

    Fix foreign key references in Message model

--- models.py
+++ models.py
@@ -65,8 +65,8 @@ class CartItem(db.Model):
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    receiver_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    sender_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    receiver_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     content = db.Column(db.Text, nullable=False)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
 
